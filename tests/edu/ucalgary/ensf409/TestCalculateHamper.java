6abc2469059af1ec46e016b597b5d0c140407bf8
Hiten M <hiten.mahalwar@ucalgary.ca>
2022-04-14 12:16:06 -0600

    Corrected Food nutrition values

--- tests/edu/ucalgary/ensf409/TestCalculateHamper.java
+++ tests/edu/ucalgary/ensf409/TestCalculateHamper.java
@@ -33,11 +33,11 @@ public class TestCalculateHamper {
 
         // Populates the inventory with some food items
         FoodItem[] foods = new FoodItem[] {
-            new FoodItem("1","Apple, dozen", new int[]{0,624,0,0,624}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
-            new FoodItem("2","Ham", new int[]{0,0,900,0,900}),              // ('Ham', 0, 0, 100, 0, 250),     
-            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),             // ('Spam', 50, 0, 50, 0, 800),
-            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,790}),       // ('Eggs, dozen', 0, 0, 9, 91, 864),
-            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}),   // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
+            new FoodItem("1","Apple, dozen", new int[]{0,700,0,0,700}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
+            new FoodItem("2","Ham", new int[]{0,0,250,0,250}),              // ('Ham', 0, 0, 100, 0, 250),     
+            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),           // ('Spam', 50, 0, 50, 0, 800),
+            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,864}),      // ('Eggs, dozen', 0, 0, 9, 91, 864),
+            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}), // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
             new FoodItem("6","Mineral Water", new int[]{0,0,0,750,750}),    // ('Mineral Water', 0, 0, 0, 100, 750),
             new FoodItem("7","Tuna", new int[]{0,0,100,0,100})              // ('Tuna', 0, 0, 100, 0, 100),
         };
@@ -50,14 +50,7 @@ public class TestCalculateHamper {
         FoodItem[] actual = actualHamper.getContents();
         // Adult male will need 400, 700, 650, 750 and 2500
         // The most efficient hamper in this case would contain "Apple, dozen", "Ham", "Spam", and "Mineral Water" in that order which would meet all needs exactly
-        Hamper expectedHamper = new Hamper( 
-                        new FoodItem[]{ foods[0], foods[1], foods[2], foods[5]}, 
-                        new Client[]{ new Client(1) }    // Client(id 1) = Adult Male (according to Project Handout)
-                        );
-        FoodItem[] expected = expectedHamper.getContents();
-
-        System.out.println("Expected: " + expected.toString());
-        System.out.println("Actual: " + actual.toString());
+        FoodItem[] expected = new FoodItem[]{ foods[0], foods[1], foods[2], foods[5]};
 
         assertArrayEquals("The calculated Hamper did not contain the correct contents", expected, actual);
     }
@@ -76,15 +69,15 @@ public class TestCalculateHamper {
         );
 
         FoodItem[] foods = new FoodItem[] {
-            new FoodItem("1","Apple, dozen", new int[]{0,624,0,0,624}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
-            new FoodItem("2","Ham", new int[]{0,0,900,0,900}),              // ('Ham', 0, 0, 100, 0, 250),     
-            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),             // ('Spam', 50, 0, 50, 0, 800),
-            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,790}),       // ('Eggs, dozen', 0, 0, 9, 91, 864),
-            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}),   // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
+            new FoodItem("1","Apple, dozen", new int[]{0,700,0,0,700}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
+            new FoodItem("2","Ham", new int[]{0,0,250,0,250}),              // ('Ham', 0, 0, 100, 0, 250),     
+            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),           // ('Spam', 50, 0, 50, 0, 800),
+            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,864}),      // ('Eggs, dozen', 0, 0, 9, 91, 864),
+            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}), // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
             new FoodItem("6","Mineral Water", new int[]{0,0,0,750,750}),    // ('Mineral Water', 0, 0, 0, 100, 750),
             new FoodItem("7","Tuna", new int[]{0,0,100,0,100})              // ('Tuna', 0, 0, 100, 0, 100),
         };
-        // As shown in the test above, this hamper has exactly what the client will need except for one extra item ( foods[6] )
+        // As shown in the test above, this hamper has exactly what the client will need except for one extra item ( Tuna foods[6] )
         // The extra item should result in exactly 100 calories of waste
         Hamper testHamper = new Hamper( 
             new FoodItem[]{ foods[0], foods[1], foods[2], foods[5], foods[6]}, 
@@ -110,14 +103,15 @@ public class TestCalculateHamper {
         );
 
         FoodItem[] foods = new FoodItem[] {
-            new FoodItem("1","Apple, dozen", new int[]{0,624,0,0,624}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
-            new FoodItem("2","Ham", new int[]{0,0,900,0,900}),              // ('Ham', 0, 0, 100, 0, 250),     
-            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),             // ('Spam', 50, 0, 50, 0, 800),
-            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,790}),       // ('Eggs, dozen', 0, 0, 9, 91, 864),
-            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}),   // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
+            new FoodItem("1","Apple, dozen", new int[]{0,700,0,0,700}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
+            new FoodItem("2","Ham", new int[]{0,0,250,0,250}),              // ('Ham', 0, 0, 100, 0, 250),     
+            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),           // ('Spam', 50, 0, 50, 0, 800),
+            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,864}),      // ('Eggs, dozen', 0, 0, 9, 91, 864),
+            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}), // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
             new FoodItem("6","Mineral Water", new int[]{0,0,0,750,750}),    // ('Mineral Water', 0, 0, 0, 100, 750),
             new FoodItem("7","Tuna", new int[]{0,0,100,0,100})              // ('Tuna', 0, 0, 100, 0, 100),
         };
+
         // As shown in the first test above, this hamper has exactly what the client will need
         // The nutrition of the hamper will be exactly [16,28,26,30,2500] note that is also the exact client needs
         Hamper testHamper = new Hamper( 
--- tests/edu/ucalgary/ensf409/TestOrder.java
+++ tests/edu/ucalgary/ensf409/TestOrder.java
@@ -18,11 +18,11 @@ public class TestOrder
     {
         // Setup Inventory for the test
         FoodItem[] foods = new FoodItem[] {
-            new FoodItem("1","Apple, dozen", new int[]{0,624,0,0,624}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
-            new FoodItem("2","Ham", new int[]{0,0,900,0,900}),              // ('Ham', 0, 0, 100, 0, 250),     
-            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),             // ('Spam', 50, 0, 50, 0, 800),
-            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,790}),       // ('Eggs, dozen', 0, 0, 9, 91, 864),
-            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}),   // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
+            new FoodItem("1","Apple, dozen", new int[]{0,700,0,0,700}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
+            new FoodItem("2","Ham", new int[]{0,0,250,0,250}),              // ('Ham', 0, 0, 100, 0, 250),     
+            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),           // ('Spam', 50, 0, 50, 0, 800),
+            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,864}),      // ('Eggs, dozen', 0, 0, 9, 91, 864),
+            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}), // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
             new FoodItem("6","Mineral Water", new int[]{0,0,0,750,750}),    // ('Mineral Water', 0, 0, 0, 100, 750),
             new FoodItem("7","Tuna", new int[]{0,0,100,0,100})              // ('Tuna', 0, 0, 100, 0, 100),
         };
@@ -62,11 +62,11 @@ public class TestOrder
     {
         // Setup Inventory for the test
         FoodItem[] foods = new FoodItem[] {
-            new FoodItem("1","Apple, dozen", new int[]{0,624,0,0,624}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
-            new FoodItem("2","Ham", new int[]{0,0,900,0,900}),              // ('Ham', 0, 0, 100, 0, 250),     
-            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),             // ('Spam', 50, 0, 50, 0, 800),
-            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,790}),       // ('Eggs, dozen', 0, 0, 9, 91, 864),
-            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}),   // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
+            new FoodItem("1","Apple, dozen", new int[]{0,700,0,0,700}),     // ('Apple, dozen', 0, 100, 0, 0, 700),
+            new FoodItem("2","Ham", new int[]{0,0,250,0,250}),              // ('Ham', 0, 0, 100, 0, 250),     
+            new FoodItem("3","Spam", new int[]{400,0,400,0,800}),           // ('Spam', 50, 0, 50, 0, 800),
+            new FoodItem("4","Eggs, dozen", new int[]{0,0,4,786,864}),      // ('Eggs, dozen', 0, 0, 9, 91, 864),
+            new FoodItem("5","Banana, bunch 5", new int[]{0,582,23,0,605}), // ('Banana, bunch 5', 0, 97, 3, 0, 605), 
             new FoodItem("6","Mineral Water", new int[]{0,0,0,750,750}),    // ('Mineral Water', 0, 0, 0, 100, 750),
             new FoodItem("7","Tuna", new int[]{0,0,100,0,100})              // ('Tuna', 0, 0, 100, 0, 100),
         };
